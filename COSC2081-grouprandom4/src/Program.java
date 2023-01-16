a683744d3fd9fc41173ed9c1d47811638c21fa25
phan281 <96297977+phan281@users.noreply.github.com>
2023-01-16 21:08:00 +0700

    Update Program.java

--- COSC2081-grouprandom4/src/Program.java
+++ COSC2081-grouprandom4/src/Program.java
@@ -1,4 +1,11 @@
-
+/*
+  RMIT University Vietnam
+  Course: COSC2081 Programming 1
+  Semester: 2022C
+  Assessment: Assignment 3
+  Author: Vu Phan Anh
+  ID: 3926005
+*/
 import java.io.IOException;
 
 import Views.Menu;
